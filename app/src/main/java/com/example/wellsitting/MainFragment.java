6ffac807664badcd3da60a887d98db984e6821fc
yemugogogo <poorclown94@gmail.com>
2019-05-08 01:31:46 +0800

    take off the title bar

--- app/src/main/java/com/example/wellsitting/MainFragment.java
+++ app/src/main/java/com/example/wellsitting/MainFragment.java
@@ -13,7 +13,6 @@ import android.support.annotation.LayoutRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.text.format.Time;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -268,7 +267,6 @@ public class MainFragment extends Fragment {
             public void onClick(View arg0) {
                 Intent intent = new Intent(mcontext,Service.class);
                 mcontext.stopService(intent);
-
                 timeremain.cancel();
 
             }

d3b557ea30e33f0bf2bb8a57ee218ae8b37ac29d
yemugogogo <poorclown94@gmail.com>
2019-05-20 07:47:05 +0800

    Able to paste, but the text color is always white.

--- app/src/main/java/com/example/wellsitting/About.java
+++ app/src/main/java/com/example/wellsitting/About.java
@@ -1,10 +1,15 @@
 package com.example.wellsitting;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class About extends AppCompatActivity {
 
+    Activity context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

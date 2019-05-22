994abe195cefa1c44c4fc57eba4cdec0b404e1b6
yemugogogo <poorclown94@gmail.com>
2019-05-22 15:07:18 +0800

    Add Splashscreen

---
+++ app/src/main/java/com/example/wellsitting/Splashscreen.java
@@ -0,0 +1,31 @@
+package com.example.wellsitting;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(1500);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+
+
+        myThread.start();
+    }
+}
